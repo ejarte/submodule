42e8f96fe2e26703fbbd9c129601b024fbbc6028
ejarte <ejarte@kth.se>
2018-06-29 18:36:00 +0100

    at last

--- theSubmoduleScript.sh
+++ theSubmoduleScript.sh
@@ -1,15 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
 valuex=$(git rev-parse HEAD)
-valuey=$(git rev-parse master) #hum locations????
+valuey=$(git rev-parse origin/master) #hum locations????
 
-#echo "this commit"
-#echo "$valuex"
-#echo "Main commit"
-#echo "$valuey"
-if [ $valuex != $valuey ] ;then
+if [ -n "$(git log ^$valuey  $valuex)" ] ;then
 	echo "warning this submodule is not on the same branch as the Main"
 	echo "$(git log ^$valuey $valuex)"
 fi
 
-#echo "end of script"
